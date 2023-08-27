d3c94455e89e69e30ff65d7bbff4c09f0342e281
arefin <arefinnuman2000@gmail.com>
2023-08-27 11:41:13 +0600

    Project started with creating terminal and ebl365 modules

--- src/app/modules/atmBooths/atmBooths.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'mongoose';
-
-export type IAtmBooth = {
-  name: string;
-};
-
-export type AtmBoothModel = Model<IAtmBooth, Record<string, unknown>>;
--- src/app/modules/atmBooths/atmBooths.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { AtmBoothsController } from './atmBooths.controller';
-
-const router = express.Router();
-
-router.post('/', AtmBoothsController.createAtmBooth);
-
-export const AtmBoothsRoutes = router;
